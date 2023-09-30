d5a42eea2a55ab2a86ebf773d929e6641a0e1105
KevinZhang <zhang.kevin333@gmail.com>
2023-09-30 00:38:48 -0700

    add rest of models

--- backend/auction/models.py
+++ backend/auction/models.py
@@ -3,3 +3,18 @@ from core.models import AuditModel
 
 class Auction(AuditModel):
     date = models.DateTimeField(editable=False)
+
+class Brand(AuditModel):
+    name = models.CharField(max_length=150, null=False)
+
+class Truck(AuditModel):
+    date = models.DateTimeField(editable=False)
+    asking_price = models.IntegerField(null=False)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+
+    auction = models.ForeignKey(Auction, on_delete=models.PROTECT)
+
+
+class Bid(AuditModel):
+    amount = models.IntegerField(null=False)
+    truck = models.ForeignKey(Truck, on_delete=models.PROTECT)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from core.models import AuditModel
+from auction.models import Auction
 # Create your models here.
 class User(AuditModel):
+    """
+        A user will become a bidder if their manytomanyfield contains the 
+        auction in question. An auction will have many bidders, and a bidder
+        will be in many auctions. 
+    """
     name = models.CharField(max_length=150, null=False)
+    email = models.CharField(max_length=150, null=False)
+    auction = models.ManyToManyField(Auction)
