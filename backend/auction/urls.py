934fd615f5ce1f287a37ed291936b25fd1246c7a
Lance Tan <ltan0521@gmail.com>
2023-11-07 16:32:47 -0800

    Add endpoints

--- backend/auction/urls.py
+++ backend/auction/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import AuctionListApiView
+from .views import AuctionListApiView, AuctionDetailApiView
+
 
 urlpatterns = [
-    path("", AuctionListApiView.as_view(), name="auction"),
+    path("", AuctionListApiView.as_view(), name="auction_list"),
+    path("<uuid:auction_id>/", AuctionDetailApiView.as_view(), name="auction_detail"),
 ]
