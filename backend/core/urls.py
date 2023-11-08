79d323fb9878ba5795ae2c808510a14ac3b9d8ff
Lance Tan <63096217+ltan02@users.noreply.github.com>
2023-11-07 18:02:50 -0800

    Add update, delete, and get auction endpoints (#14)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin", admin.site.urls),
-    path("api/v1/auction", include("auction.urls")),
-    path("api/v1/vehicle", include("vehicle.urls")),
+    path("admin/", admin.site.urls),
+    path("api/v1/auction/", include("auction.urls")),
+    path("api/v1/vehicle/", include("vehicle.urls")),
 ]
