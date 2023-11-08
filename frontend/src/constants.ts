4fb4c6d9334ce18f5e1af7908df7b0f7fcd30c20
Lance Tan <ltan0521@gmail.com>
2023-11-07 23:00:14 -0800

    Update build pipeline

---
+++ frontend/src/constants.ts
@@ -0,0 +1,6 @@
+export const APP_COLOR = {};
+
+const developmentBackendUrl = "http://127.0.0.1:8000";
+const productionBackendUrl = "";
+
+export const BACKEND_URL = process.env.NODE_ENV === "development" ? developmentBackendUrl : productionBackendUrl;
