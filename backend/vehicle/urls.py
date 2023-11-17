c6f66887f0a9fcbe27aa4833808c9ab7ccbc4e30
Brian Park <brianpark1208@gmail.com>
2023-11-17 14:23:15 -0800

    added vehicledetail view and updated endpoints

--- backend/vehicle/urls.py
+++ backend/vehicle/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import VehicleListApiView
+from .views import VehicleListApiView, VehicleDetailApiView
 
 urlpatterns = [
     path("", VehicleListApiView.as_view(), name="vehicle"),
+    path("<uuid:vehicle_id>/", VehicleDetailApiView.as_view(), name="vehicle_detail"),
 ]
