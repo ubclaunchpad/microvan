34859861d4ddc774b8dad9cdb8efdcd72b9c8007
Lance Tan <63096217+ltan02@users.noreply.github.com>
2023-11-18 13:50:53 -0800

    Update frontend to plain javascript (#17)

--- frontend/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const APP_COLOR = {};
-
-const developmentBackendUrl = "http://127.0.0.1:8000";
-const productionBackendUrl = "";
-
-export const BACKEND_URL = process.env.NODE_ENV === "development" ? developmentBackendUrl : productionBackendUrl;
-
-console.log(BACKEND_URL)
