9607d33900a1187dcbff41ed33b9c110963e10c7
nikkilaude <nikkilaudee@gmail.com>
2024-01-14 17:41:49 -0800

    update vehicle

--- backend/vehicle/views.py
+++ backend/vehicle/views.py
@@ -70,6 +70,19 @@ class VehicleDetailApiView(APIView):
         except vehicle.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+    def put(self, request, vehicle_id, format=None):
+        """
+        Update specific vehicle
+
+        """
+        vehicle = get_object_or_404(Vehicle, id=vehicle_id)
+        serializer = VehicleSerializer(
+            vehicle, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, vehicle_id, format=None):
         """
         Delete specific vehicle
