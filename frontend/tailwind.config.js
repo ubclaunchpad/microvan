2a6a6aef170aa1a4a963009a10291c4f43d671f6
Lance Tan <ltan0521@gmail.com>
2024-02-02 23:12:47 -0800

    Create nav bar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
 			},
 			boxShadow: {
 				buttonShadow: '0px 4px 16px 0px rgba(22, 22, 22, 0.10)',
+				navBarShadow: '0px 2px 4px 0px rgba(22, 22, 22, 0.10)',
 			},
 		},
 	},
