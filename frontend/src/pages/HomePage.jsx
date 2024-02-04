dca76b64f750cae57b80fa9810e211dc867f2db8
Lance Tan <ltan0521@gmail.com>
2024-02-03 18:01:30 -0800

    Remove testing button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
 import NavBar from '../components/nav-bar/NavBar';
 
 export default function HomePage() {
-	const navigate = useNavigate();
-
 	return (
 		<div className="App">
 			<NavBar />
-			<button type="button" onClick={() => navigate('/onboard')}>
-				Onboard
-			</button>
 		</div>
 	);
 }
