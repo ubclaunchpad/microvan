352cc9c0adf71398a7e05b3b9c8ee313966e2370
_Kevin_Zhang_ <49049813+Dragollax@users.noreply.github.com>
2024-02-11 17:49:56 -0800

    remove username option for signin

--- backend/user/backends.py
+++ backend/user/backends.py
@@ -6,7 +6,7 @@ from .models import User
 class EmailorUsernameModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            user = User.objects.get(Q(email__iexact=username))
         except User.DoesNotExist:
             return None
         else:
