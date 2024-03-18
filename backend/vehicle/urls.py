c1b8521eb7daaf8a7c7e30111aa84b281a639b5b
Lance Tan <63096217+ltan02@users.noreply.github.com>
2024-03-18 12:35:50 -0700

    Set up Lambda for custom emails (#57)

--- backend/vehicle/urls.py
+++ backend/vehicle/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from .views import (
+    UploadFileView,
     VehicleDetailApiView,
     VehicleFilterList,
     VehicleListApiView,
     VehiclePriceApiView,
-    UploadFileView,
 )
 
 urlpatterns = [
