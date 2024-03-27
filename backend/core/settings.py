59affc4d020b77f1a24d502662ad418703720e11
_Kevin_Zhang_ <49049813+Dragollax@users.noreply.github.com>
2024-03-27 15:09:13 -0700

    add back env code

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import environ
+
+# Reads environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
