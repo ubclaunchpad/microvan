286e12695ce0c2d8ae08d69fc13d2253d995a3f7
_Kevin_Zhang_ <49049813+Dragollax@users.noreply.github.com>
2024-03-27 15:32:23 -0700

    remove code

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
