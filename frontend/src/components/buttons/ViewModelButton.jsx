bc6023fe3b44c65765e2702c8d62e33459186471
Lance Tan <ltan0521@gmail.com>
2024-04-09 21:30:24 -0700

    Implement quick view modal

--- frontend/src/components/buttons/ViewModelButton.jsx
+++ frontend/src/components/buttons/ViewModelButton.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-export default function ViewModelButton({ onClick }) {
+export default function ViewModelButton({ onClick, size = 'sm' }) {
+	const buttonStyle = {
+		sm: 'px-[29px] py-[12.5px]',
+		lg: 'px-[76px] py-[15px]',
+	};
+
+	const textStyle = {
+		sm: 'text-sm',
+		lg: 'text-base',
+	};
+
 	return (
 		<button
 			type="button"
-			className="bg-mv-green border-solid border-mv-green rounded-[5px] w-full shadow-searchBarShadow px-[29px] py-[12.5px] flex items-center justify-center gap-2.5"
+			className={`bg-mv-green border-solid border-mv-green rounded-[5px] shadow-searchBarShadow flex items-center justify-center gap-2.5 ${buttonStyle[size]}`}
 			onClick={onClick}
 		>
-			<div className="text-mv-white text-sm font-medium">View model</div>
+			<div className={`text-mv-white font-medium ${textStyle[size]}`}>
+				View model
+			</div>
 		</button>
 	);
 }
--- frontend/src/components/cards/VehicleItemCard.jsx
+++ frontend/src/components/cards/VehicleItemCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import AgricultureIcon from '@mui/icons-material/Agriculture';
@@ -6,6 +6,7 @@ import StarsOutlinedIcon from '@mui/icons-material/StarsOutlined';
 import AddToListButton from '../buttons/AddToListButton';
 import ViewModelButton from '../buttons/ViewModelButton';
 import QuickViewButton from '../buttons/QuickViewButton';
+import QuickViewModal from '../modals/QuickViewModal';
 
 export default function VehicleItemCard({
 	vehicleId,
@@ -16,6 +17,8 @@ export default function VehicleItemCard({
 	price,
 	imageUrl,
 }) {
+	const [isQuickViewOpen, setIsQuickViewOpen] = useState(false);
+
 	const navigate = useNavigate();
 
 	const handleViewClick = () => {
@@ -23,87 +26,101 @@ export default function VehicleItemCard({
 	};
 
 	return (
-		<div className="flex w-full h-[314px] rounded-[20px] shadow-searchBarShadow">
-			<div className="w-1/3 relative rounded-l-[20px]">
-				<img
-					className="w-full h-full rounded-l-[20px] object-cover object-center"
-					src={imageUrl}
-					alt={description}
-				/>
-				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-					<QuickViewButton />
-				</div>
-			</div>
-			<div className="w-2/3 flex pl-[46px] pt-[42px] pb-[30px] pr-[36px]">
-				<div className="w-[70%] flex flex-col gap-y-[21px]">
-					<div
-						className="font-semibold text-xl text-mv-black tracking-[0.5px] leading-7"
-						style={{
-							display: '-webkit-box',
-							WebkitLineClamp: 3,
-							WebkitBoxOrient: 'vertical',
-							overflow: 'hidden',
-							textOverflow: 'ellipsis',
-						}}
-					>
-						{description}
+		<>
+			<div className="flex w-full h-[314px] rounded-[20px] shadow-searchBarShadow">
+				<div className="w-1/3 relative rounded-l-[20px]">
+					<img
+						className="w-full h-full rounded-l-[20px] object-cover object-center"
+						src={imageUrl}
+						alt={description}
+					/>
+					<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+						<QuickViewButton onClick={() => setIsQuickViewOpen(true)} />
 					</div>
+				</div>
+				<div className="w-2/3 flex pl-[46px] pt-[42px] pb-[30px] pr-[36px]">
+					<div className="w-[70%] flex flex-col gap-y-[21px]">
+						<div
+							className="font-semibold text-xl text-mv-black tracking-[0.5px] leading-7"
+							style={{
+								display: '-webkit-box',
+								WebkitLineClamp: 3,
+								WebkitBoxOrient: 'vertical',
+								overflow: 'hidden',
+								textOverflow: 'ellipsis',
+							}}
+						>
+							{description}
+						</div>
 
-					<div className="flex flex-col text-mv-black text-base mr-[50px] gap-y-[20px]">
-						<div className="flex flex-row justify-between">
-							<div className="flex flex-row items-center justify-center gap-x-[19px]">
-								<DirectionsBusIcon
-									className="text-dark-grey"
-									sx={{ fontSize: 25 }}
-								/>
-								<div className="flex flex-col leading-6 tracking-[0.5px]">
-									<p className="font-medium">Model</p>
-									<p className="font-normal">{modelNumber}</p>
+						<div className="flex flex-col text-mv-black text-base mr-[50px] gap-y-[20px]">
+							<div className="flex flex-row w-[100%] justify-between">
+								<div className="flex flex-row items-center justify-center gap-x-[19px]">
+									<DirectionsBusIcon
+										className="text-dark-grey"
+										sx={{ fontSize: 25 }}
+									/>
+									<div className="flex flex-col leading-6 tracking-[0.5px]">
+										<p className="font-medium">Model</p>
+										<p className="font-normal">{modelNumber}</p>
+									</div>
+								</div>
+								<div className="flex flex-row items-center justify-center gap-x-[19px]">
+									<StarsOutlinedIcon
+										className="text-dark-grey"
+										sx={{ fontSize: 25 }}
+									/>
+									<div className="flex flex-col leading-6 tracking-[0.5px]">
+										<p className="font-medium">Engine no.</p>
+										<p className="font-normal">{engineNumber}</p>
+									</div>
 								</div>
 							</div>
-							<div className="flex flex-row items-center justify-center gap-x-[19px]">
-								<StarsOutlinedIcon
-									className="text-dark-grey"
-									sx={{ fontSize: 25 }}
-								/>
-								<div className="flex flex-col leading-6 tracking-[0.5px]">
-									<p className="font-medium">Engine no.</p>
-									<p className="font-normal">{engineNumber}</p>
-								</div>
-							</div>
-						</div>
-						<div className="flex flex-row justify-between">
-							<div className="flex flex-row items-center justify-center gap-x-[19px]">
-								<AgricultureIcon
-									className="text-dark-grey"
-									sx={{ fontSize: 25 }}
-								/>
-								<div className="flex flex-col leading-6 tracking-[0.5px]">
-									<p className="font-medium">Chassis</p>
-									<p className="font-normal">{chassisNumber}</p>
+							<div className="flex flex-row justify-between">
+								<div className="flex flex-row items-center justify-center gap-x-[19px]">
+									<AgricultureIcon
+										className="text-dark-grey"
+										sx={{ fontSize: 25 }}
+									/>
+									<div className="flex flex-col leading-6 tracking-[0.5px]">
+										<p className="font-medium">Chassis</p>
+										<p className="font-normal">{chassisNumber}</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="w-[30%] flex flex-col items-center justify-between gap-y-[13px]">
-					<span>
-						<p className="text-mv-black text-base font-normal leading-5 tracking-[0.1px]">
-							Current bid
-						</p>
-					</span>
-					<span className="mb-[5px]">
-						<p className="text-mv-black text-[28px] font-medium leading-5 tracking-[0.1px]">{`₱${price}`}</p>
-					</span>
-					<ViewModelButton onClick={handleViewClick} />
-					<span className="mb-[2px]">
-						<p className="text-base text-mv-black font-medium leading-5 tracking-[0.1px]">
-							or
-						</p>
-					</span>
-					<AddToListButton />
+					<div className="w-[30%] flex flex-col items-center justify-between gap-y-[13px]">
+						<span>
+							<p className="text-mv-black text-base font-normal leading-5 tracking-[0.1px]">
+								Current bid
+							</p>
+						</span>
+						<span className="mb-[5px]">
+							<p className="text-mv-black text-[28px] font-medium leading-5 tracking-[0.1px]">{`₱${price}`}</p>
+						</span>
+						<ViewModelButton onClick={handleViewClick} />
+						<span className="mb-[2px]">
+							<p className="text-base text-mv-black font-medium leading-5 tracking-[0.1px]">
+								or
+							</p>
+						</span>
+						<AddToListButton />
+					</div>
 				</div>
 			</div>
-		</div>
+			{isQuickViewOpen && (
+				<QuickViewModal
+					isOpen={isQuickViewOpen}
+					onClose={() => setIsQuickViewOpen(false)}
+					id={vehicleId}
+					description={description}
+					modelNumber={modelNumber}
+					chassisNumber={chassisNumber}
+					engineNumber={engineNumber}
+					price={price}
+				/>
+			)}
+		</>
 	);
 }
