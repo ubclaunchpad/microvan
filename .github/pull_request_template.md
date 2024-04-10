f1ab7278e6a7f76fb9060ceee2c66d395fccbc5e
Lance Tan <63096217+ltan02@users.noreply.github.com>
2024-04-09 17:17:10 -0700

    Implement basic profile page (#74)

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -20,3 +20,5 @@ Fixes # (ticket #)
 - [ ] My changes generate no new warnings
 - [ ] I have added tests that prove my fix is effective or that my feature works
 - [ ] New and existing unit tests pass locally with my changes
+
+/assignme
---
+++ frontend/src/components/cards/SavedItemCard.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
+import StarsOutlinedIcon from '@mui/icons-material/StarsOutlined';
+import BidNowButton from '../buttons/BidNowButton';
+import RemoveFromListButton from '../buttons/RemoveFromListButton';
+
+export default function SavedItemCard({
+	// eslint-disable-next-line no-unused-vars
+	vehicleId,
+	description,
+	modelNumber,
+	engineNumber,
+	chassisNumber,
+	price,
+	imageUrl,
+}) {
+	return (
+		<div className="flex w-full h-[294px] rounded-[18px] shadow-searchBarShadow">
+			<div className="w-1/3 relative rounded-l-[18px]">
+				<img
+					className="w-full h-full rounded-l-[18px] object-cover object-center"
+					src={imageUrl}
+					alt={description}
+				/>
+			</div>
+			<div className="w-2/3 flex pl-[44px] pt-[39px] pb-[26px] pr-[34px]">
+				<div className="w-[70%] flex flex-col gap-y-[24px]">
+					<div
+						className="font-semibold text-lg text-mv-black tracking-[0.5px] leading-7"
+						style={{
+							display: '-webkit-box',
+							WebkitLineClamp: 3,
+							WebkitBoxOrient: 'vertical',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+						}}
+					>
+						{description}
+					</div>
+
+					<div className="flex flex-col text-mv-black text-sm mr-[50px] gap-y-[21px]">
+						<div className="flex flex-row justify-between">
+							<div className="flex flex-row items-center justify-center gap-x-[19px]">
+								<DirectionsBusIcon
+									className="text-dark-grey"
+									sx={{ fontSize: 25 }}
+								/>
+								<div className="flex flex-col leading-6 tracking-[0.5px]">
+									<p className="font-medium">Model</p>
+									<p className="font-normal">{modelNumber}</p>
+								</div>
+							</div>
+							<div className="flex flex-row items-center justify-center gap-x-[19px]">
+								<StarsOutlinedIcon
+									className="text-dark-grey"
+									sx={{ fontSize: 25 }}
+								/>
+								<div className="flex flex-col leading-6 tracking-[0.5px]">
+									<p className="font-medium">Engine no.</p>
+									<p className="font-normal">{engineNumber}</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-row justify-between">
+							<div className="flex flex-row items-center justify-center gap-x-[19px]">
+								<AgricultureIcon
+									className="text-dark-grey"
+									sx={{ fontSize: 25 }}
+								/>
+								<div className="flex flex-col leading-6 tracking-[0.5px]">
+									<p className="font-medium">Chassis</p>
+									<p className="font-normal">{chassisNumber}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="w-[34%] flex flex-col items-center">
+					<p className="text-mv-black text-sm font-normal leading-5 tracking-[0.1px]">
+						Highest bid:
+					</p>
+					<p className="text-mv-black text-[26px] font-medium leading-5 tracking-[0.1px] mt-[12px]">{`₱${price}`}</p>
+					<div className="mt-[40px] w-[100%] flex flex-col gap-y-[14px]">
+						<BidNowButton onClick={() => {}} size="sm" />
+						<RemoveFromListButton size="sm" onClick={() => {}} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
